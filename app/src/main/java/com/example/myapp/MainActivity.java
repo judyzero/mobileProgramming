88e204cf1ae436bb2f85c6ab9478fd0fe1c29ac0
judyzero <qozmcll@kookmin.ac.kr>
2022-10-28 00:09:25 +0900

    Third 화면 구현, intent 구현

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnLogin, btnJoin;
+    Button btnLogin, btnJoin, btnProduct;
     EditText loginID, loginPassword;
+    RadioButton autoLogin;
+
+//    SharedPreferences.Editor editor;
+//    SharedPreferences memberPref;
+
+    //기록할 xml 파일 이름, 0:r,w 권한
+//    memberPref = getSharedPreferences("setting", 0);
+
+//    editor= memberPref.edit();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnJoin = (Button) findViewById(R.id.btnJoin);
+        btnProduct = (Button) findViewById(R.id.btnProduct);
 
         //로그인 눌렀을 경우
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                 //id, password값 받기
                 loginID = (EditText) findViewById(R.id.loginID);
                 loginPassword = (EditText) findViewById(R.id.loginPassword);
+                autoLogin = (RadioButton) findViewById(R.id.autoLogin);
             }
         });
 
@@ -45,6 +58,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnProduct.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),
+                        ProductActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //자동로그인 기능
+//        if(autoLogin.isChecked()){
+//            String id = loginID.getText().toString();
+//            String password = loginPassword.getText().toString();
+//
+//            editor.putString("id", id);
+//            editor.putString("password", password);
+//            editor.putBoolean("autoLogin_enabled", true);
+//            editor.apply();
+//        } else{
+//            editor.remove("id");
+//            editor.remove("pw");
+//            editor.remove("autoLogin_enabled");
+//            editor.clear();
+//            editor.apply();
+//        }
+
+        //자동로그인 - 껐다 켜도 값 유지
+//        if(setting.getBoolean("autoLogin_enabled", false)){
+//            loginID.setText(setting.getString("ID", ""));
+//            loginPassword.setText(setting.getString("PW", ""));
+//            autoLogin.setChecked(true);
+//
+//        }
 
     }
 }
--- app/src/main/java/com/example/myapp/joinActivity.java
+++ app/src/main/java/com/example/myapp/joinActivity.java
@@ -8,14 +8,19 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class joinActivity extends MainActivity {
     Button btnSave;
     EditText joinId, joinPassword, joinName, joinPhone, joinAddress;
     String id, password, name, phone, address;
     RadioButton agreeRb;
 
-    SharedPreferences memberPref, profilePref;
+    SharedPreferences memberPref;
     SharedPreferences.Editor editor;
+
+//    String pattern = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
+//    boolean digit = false;
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
@@ -23,17 +28,17 @@ public class joinActivity extends MainActivity {
         setContentView(R.layout.activity_join);
 
         btnSave = (Button) findViewById(R.id.btnSave);
+        joinId = (EditText) findViewById(R.id.joinID);
+        joinPassword = (EditText) findViewById(R.id.joinPassword);
+        joinName = (EditText) findViewById(R.id.joinName);
+        joinPhone = (EditText) findViewById(R.id.joinPhone);
+        joinAddress = (EditText) findViewById(R.id.joinAddress);
+        agreeRb = (RadioButton) findViewById(R.id.agreeRb);
 
         //회원가입하기 버튼을 눌렀을 때
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                joinId = (EditText) findViewById(R.id.joinID);
-                joinPassword = (EditText) findViewById(R.id.joinPassword);
-                joinName = (EditText) findViewById(R.id.joinName);
-                joinPhone = (EditText) findViewById(R.id.joinPhone);
-                joinAddress = (EditText) findViewById(R.id.joinAddress);
-
                 id = joinId.getText().toString(); //아이디
                 password = joinPassword.getText().toString(); //비밀번호
                 name = joinName.getText().toString();
@@ -45,16 +50,44 @@ public class joinActivity extends MainActivity {
                     Toast.makeText(getApplicationContext(), "약관을 읽고 동의 해주세요.",
                             Toast.LENGTH_SHORT).show();
                 }
-                else if(id.equals("") || password.equals("") || name.equals("")
-                        || phone.equals("") || address.equals("")){
-                    Toast.makeText(getApplicationContext(), "빈칸을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+
+                //빈칸이면 쓰도록
+                else if(id.equals("")){
+                    Toast.makeText(getApplicationContext(), "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show();
                 }
+
+                else if(password.equals("")){
+                    Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(name.equals("")){
+                    Toast.makeText(getApplicationContext(), "이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(phone.equals("")){
+                    Toast.makeText(getApplicationContext(), "전화번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+
+                else if(address.equals("")){
+                    Toast.makeText(getApplicationContext(), "주소를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+
+                //비밀번호 자릿수
+                else if(password.length() < 5) {
+                    Toast.makeText(getApplicationContext(), "비밀번호는 5자 이상 입력해야 합니다.", Toast.LENGTH_SHORT).show();
+                }
+
+//                //특수키 규칙 확인
+//                else if(Pattern.matches(pattern, password)) {
+//                    Toast.makeText(getApplicationContext(), "특수키를 입력해주세요.", Toast.LENGTH_SHORT).show();
+//                }
+
                 else{
                     //회원가입 정보를 모두 입력했다면 회원가입 정보 쉐어드에 저장
                     signCheck();
+                   Toast.makeText(getApplicationContext(), "회원가입을 성공했습니다.", Toast.LENGTH_SHORT).show();
+                   finish();
                 }
-                //다시 mainActivity로 돌아감
-                finish();
             }
         });
 
@@ -62,12 +95,14 @@ public class joinActivity extends MainActivity {
 
     //회원가입정보 프레퍼런스에 저장
     private void  signCheck(){
-        memberPref = this.getSharedPreferences("memberPref", MainActivity.MODE_PRIVATE);
+        memberPref = this.getSharedPreferences("memberPref", joinActivity.MODE_PRIVATE);
         editor = memberPref.edit();
-        editor.putString(id, password);
-        editor.putString(name, phone);
+        editor.putString("id", id);
+        editor.putString("password", password);
+        editor.putString("name", name);
+        editor.putString("phone", phone);
+        editor.putString("address", address);
         this.editor.apply();
     }
-
 }
 
